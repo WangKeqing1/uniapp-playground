50c6d919c31affcb07972e64fd12554fec0b0f5b
王可青 <1139719961@qq.com>
2026-03-03 18:00:06 +0800

    feat: 添加 Electron 跨端支持
    - 集成 vite-plugin-electron 插件
    - 添加 Electron 主进程入口文件主逻辑
    - 配置 Vite 以支持桌面端编译与运行

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import electron from 'vite-plugin-electron/simple'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
-});
+  plugins: [uni(), electron({ main: { entry: 'electron/main.ts' } })],
+})
